98b07c33be56d6bf7d487759a3461c069d04f762
Tushar Sharma <sharma.tu@husky.neu.edu>
2019-10-22 16:16:49 -0400

    Update coinchange.py

--- coinchange.py
+++ coinchange.py
@@ -1,39 +1,60 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Oct 20 05:52:19 2019
+#!/bin/python3
 
-@author: Tushar
-"""
+import math
+import os
+import random
+import re
+import sys
 
+#
+# Complete the 'getWays' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. LONG_INTEGER_ARRAY c
+#
 
-# Recursive Python3 program for 
-# coin change problem. 
+def getWays(n, m, S):
+    # We need n+1 rows as the table is constructed  
+    # in bottom up manner using the base case 0 value 
+    # case (n = 0) 
+    table = [[0 for x in range(m)] for x in range(n+1)] 
   
-# Returns the count of ways we can sum 
-# S[0...m-1] coins to get sum n 
-def count(S, m, n ): 
+    # Fill the entries for 0 value case (n = 0) 
+    for i in range(m): 
+        table[0][i] = 1
   
-    # If n is 0 then there is 1 
-    # solution (do not include any coin) 
-    if (n == 0): 
-        return 1
+    # Fill rest of the table entries in bottom up manner 
+    for i in range(1, n+1): 
+        for j in range(m): 
   
-    # If n is less than 0 then no 
-    # solution exists 
-    if (n < 0): 
-        return 0; 
+            # Count of solutions including S[j] 
+            x = table[i - S[j]][j] if i-S[j] >= 0 else 0
   
-    # If there are no coins and n 
-    # is greater than 0, then no 
-    # solution exist 
-    if (m <=0 and n >= 1): 
-        return 0
+            # Count of solutions excluding S[j] 
+            y = table[i][j-1] if j >= 1 else 0
   
-    # count is sum of solutions (i)  
-    # including S[m-1] (ii) excluding S[m-1] 
-    return count( S, m - 1, n ) + count( S, m, n-S[m-1] );
+            # total count 
+            table[i][j] = x + y 
+  
+    return table[n][m-1] 
 
-# Driver program to test above function 
-arr = [1, 2, 3] 
-m = len(arr) 
-print(count(arr, m, 4))
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    c = list(map(int, input().rstrip().split()))
+
+    # Print the number of ways of making change for 'n' units using coins having the values given by 'c'
+
+    ways = getWays(n, len(c), c)
+
+    fptr.write(str(ways) + '\n')
+
+    fptr.close()
